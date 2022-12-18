cd59800b46af5c3be223db027a6fd122373db23d
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-19 00:10:06 +0200

    додає розмітку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector(".js-gallery");
+const markup = galleryItems.reduce((acc, {
+    preview,
+    original,
+    description,
+}) => acc +
+`<div class="gallery__item">
+<a class="gallery__link" href=${original}>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source= ${original}
+    alt=${description}
+  />
+</a>
+</div>`, "");
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
 console.log(galleryItems);
